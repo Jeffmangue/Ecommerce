7b8b733143e4be0f7af690ba95c505d7f0601dc3
Ecommerce <manguejefferson@gmail.com>
2023-10-11 23:53:50 +0800

    vc

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='login/reset_password.html'), name="reset_password"),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name='login/sent_reset_link.html'), name="password_reset_done"),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="login/reset_complete.html"), name="password_reset_complete"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='login/reset_confirm.html'), name="password_reset_confirm"),
     path('admin/', admin.site.urls),
     path('',include('Homepage.urls'))
     path('', include('login.urls')),
