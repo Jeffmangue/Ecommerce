ab55f041b0a3eb4888736b83ac22c4a03051028a
Ecommerce <manguejefferson@gmail.com>
2023-10-11 22:35:31 +0800

    check

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j$1d1i30b&9*%w6hgtu36*z79cbnz#ib-^6v!vh%m*fz#8d#mg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mirawil.com']
 
 
 # Application definition
